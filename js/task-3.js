2083363bd9d8c2f5a1386366c3444d04772b57b5
Eone-tgx <eone1024@gmail.com>
2024-12-05 00:23:44 +0200

    hw js 2

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
-function getElementWidth(content, padding, border) {
-  const cont = parseFloat(content);
-  const padd = parseFloat(padding);
-  const bordr = parseFloat(border);
-  return cont + padd * 2 + bordr * 2;
+function checkForSpam(message) {
+  const messageLowerCase = message.toLowerCase();
+  return messageLowerCase.includes("sale") || messageLowerCase.includes("spam");
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
